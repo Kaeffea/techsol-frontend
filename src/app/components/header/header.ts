2683647eeb0640989facaded4a8c979a4a9b85bf
Kaeffea <kaeffea@gmail.com>
2025-06-26 23:00:13 -0300

    feat: implementa estrutura visual do header e footer com Angular Material

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'; // Para ícones
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatToolbarModule, // Módulo da Toolbar
+    MatButtonModule, // Módulo dos Botões
+    MatIconModule,
+  ],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
